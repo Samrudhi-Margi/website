ce91367d5a546d2307c4c501a31ad70d23934b5a
Samrudhi Margi <11421370+Samrudhi-Margi@users.noreply.github.com>
2022-09-29 22:53:53 +0530

    upload new website

---
+++ js/main.js
@@ -0,0 +1,120 @@
+
+(function() {
+    "use strict";
+  
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+      el = el.trim()
+      if (all) {
+        return [...document.querySelectorAll(el)]
+      } else {
+        return document.querySelector(el)
+      }
+    }
+  
+    /**
+     * Easy on scroll event listener 
+     */
+    const onscroll = (el, listener) => {
+      el.addEventListener('scroll', listener)
+    }
+  
+    /**
+     * Scrolls to an element with header offset
+     */
+    const scrollto = (el) => {
+      let header = select('#header')
+      let offset = header.offsetHeight
+  
+      let elementPos = select(el).offsetTop
+      window.scrollTo({
+        top: elementPos - offset,
+        behavior: 'smooth'
+      })
+    }
+  
+    /**
+     * Toggle .header-scrolled class to #header when page is scrolled
+     */
+    let selectHeader = select('#header')
+    let selectTopbar = select('#topbar')
+    if (selectHeader) {
+      const headerScrolled = () => {
+        if (window.scrollY > 100) {
+          selectHeader.classList.add('header-scrolled')
+          if (selectTopbar) {
+            selectTopbar.classList.add('topbar-scrolled')
+          }
+        } else {
+          selectHeader.classList.remove('header-scrolled')
+          if (selectTopbar) {
+            selectTopbar.classList.remove('topbar-scrolled')
+          }
+        }
+      }
+      window.addEventListener('load', headerScrolled)
+      onscroll(document, headerScrolled)
+    }
+  
+    // webinar slider
+    let items = document.querySelectorAll('#webinar .carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+// team slider
+// let items1 = document.querySelectorAll('#team .carousel .carousel-item')
+
+// items1.forEach((el) => {
+//     const minPerSlide = 4
+//     let next = el.nextElementSibling
+//     for (var i=1; i<minPerSlide; i++) {
+//         if (!next) {
+//             // wrap carousel by using first child
+//         	next = items1[0]
+//       	}
+//         let cloneChild = next.cloneNode(true)
+//         el.appendChild(cloneChild.children[0])
+//         next = next.nextElementSibling
+//     }
+// })
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav: false,
+  dots: true,
+  autoplay:true,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          dots:true,
+          nav:false
+      },
+      600:{
+          items:3,
+          dots:true,
+          nav:false
+      },
+      1000:{
+          items:4,
+          dots:true,
+          loop:true
+      }
+  }
+})
+   
+  
+  })()
